7d1f76bd43a07e8e658f6972b2cc0d21a8fb47f7
chachacollins <collinschacha@hotmail.com>
2025-05-09 14:42:55 +0300

    feat: added regex to extract code blocks from markdown files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -22,6 +23,33 @@ func ExtractCodeBlocks(md string) []string {
 	return results
 }
 
+func usage() {
+	fmt.Println("[Usage]: codex <input> <output>")
+}
+
 func main() {
-	fmt.Println("Hello world")
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Fprintf(os.Stderr, "[Error]: not enough options provided\n")
+		usage()
+		os.Exit(1)
+	}
+	input := args[1]
+	output := args[2]
+	inputFile, err := os.ReadFile(input)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Error]: %s", err)
+		os.Exit(1)
+	}
+	codeBlock := ExtractCodeBlocks(string(inputFile))
+	outputFile, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Error]: %s", err)
+		os.Exit(1)
+	}
+	defer outputFile.Close()
+	for _, code := range codeBlock {
+		outputFile.WriteString(code + "\n")
+	}
+	fmt.Printf("[success]: compiled %s to %s\n", input, output)
 }
